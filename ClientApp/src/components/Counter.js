7aa19c5429477be6382791147b40175deeaebc1f
Jmnoland <jody-noreply@git.com>
2021-01-30 19:29:34 +0200

    eslint configuration setup

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Counter extends Component {
-  static displayName = Counter.name;
-
   constructor(props) {
     super(props);
     this.state = { currentCount: 0 };
@@ -11,7 +9,7 @@ export class Counter extends Component {
 
   incrementCounter() {
     this.setState({
-      currentCount: this.state.currentCount + 1
+      currentCount: this.state.currentCount + 1,
     });
   }
 
@@ -22,9 +20,13 @@ export class Counter extends Component {
 
         <p>This is a simple example of a React component.</p>
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+        <p aria-live="polite">
+          Current count: <strong>{this.state.currentCount}</strong>
+        </p>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <button className="btn btn-primary" onClick={this.incrementCounter}>
+          Increment
+        </button>
       </div>
     );
   }
