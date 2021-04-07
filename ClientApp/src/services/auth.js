cdcc70958e34056c079e646ef3845e8d185b1810
Jmnoland <jody-noreply@git.com>
2021-04-07 23:00:28 +0200

    Added favicon

--- ClientApp/src/services/auth.js
+++ ClientApp/src/services/auth.js
@@ -9,14 +9,14 @@ async function CookiesSupported() {
 
 export async function UserLogin(email, password) {
   const { data } = await api.post(`${controller}/login`, { email, password });
-  const cookiesAllowed = await CookiesSupported();
+  //   const cookiesAllowed = await CookiesSupported();
   localStorage.setItem("X-User-Token", data);
   return data;
 }
 
 export async function DemoLogin() {
   const { data } = await api.post(`${controller}/demologin`);
-  const cookiesAllowed = await CookiesSupported();
+  //   const cookiesAllowed = await CookiesSupported();
   localStorage.setItem("X-User-Token", data);
   return data;
 }
