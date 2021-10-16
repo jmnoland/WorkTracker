6d824894d011e43d079fafc55d3cc0063b52473f
Jmnoland <jody-noreply@git.com>
2021-10-16 22:24:55 +0200

    Added return types

--- WorkTracker/ClientApp/src/pages/board/functions.ts
+++ WorkTracker/ClientApp/src/pages/board/functions.ts
@@ -13,7 +13,7 @@ export function getMaxHeight(
   return `${ch - hh - fh - 22 - 20}px`;
 }
 
-export function createNewTask(taskCount: number, storyId?: number | null) {
+export function createNewTask(taskCount: number, storyId?: number | null): Task {
   return {
     taskId: taskCount + 1,
     storyId: storyId ?? 0,
@@ -23,7 +23,7 @@ export function createNewTask(taskCount: number, storyId?: number | null) {
   };
 }
 
-export function handleTaskChange(tasks: Task[], taskId: number, value: string) {
+export function handleTaskChange(tasks: Task[], taskId: number, value: string): Task[] {
   const temp = tasks.find((task) => task.taskId === taskId);
   const items = tasks.reduce((total, task) => {
     if (task.taskId !== taskId) total.push(task);
@@ -33,7 +33,7 @@ export function handleTaskChange(tasks: Task[], taskId: number, value: string) {
   return items;
 }
 
-export function parseTasks(tasks: Task[]) {
+export function parseTasks(tasks: Task[]) : Task[] {
   return tasks &&
   tasks.reduce((total, task) => {
     const desc = task.description && task.description.trim();
@@ -42,20 +42,20 @@ export function parseTasks(tasks: Task[]) {
   }, [] as Task[]);
 }
 
-export async function removeTaskById(tasks: Task[], taskId: number) {
+export async function removeTaskById(tasks: Task[], taskId: number): Promise<Task[]> {
   const taskToRemove = tasks.filter((task) => task.taskId === taskId)[0];
   if (taskToRemove && !taskToRemove.new) await DeleteTask(taskId);
   return [...tasks.filter((task) => task.taskId !== taskId)];
 }
 
-export function reorder(list: Story[], startIndex: number, endIndex: number) {
+export function reorder(list: Story[], startIndex: number, endIndex: number): Story[] {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
   return result;
 };
 
-export function createPayload(stateId: string | number, storyList: Story[]) {
+export function createPayload(stateId: string | number, storyList: Story[]): { stateId: number, stories: Dictionary<number> }{
   const temp = storyList.reduce((total, story, index) => {
     if (story.storyId !== undefined) total[story.storyId] = index;
     return total;
