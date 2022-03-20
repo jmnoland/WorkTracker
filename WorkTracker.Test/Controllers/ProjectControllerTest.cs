b42fdbbf219ce390bae83fd2ee746c8c767662ca
Jmnoland <jody-noreply@git.com>
2022-03-20 18:44:01 +0200

    Added Project service tests

--- WorkTracker.Test/Controllers/ProjectControllerTest.cs
+++ WorkTracker.Test/Controllers/ProjectControllerTest.cs
@@ -1,24 +1,80 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
 using WorkTracker.Controllers;
-using WorkTracker.Models.Requests;
+using WorkTracker.Models;
+using WorkTracker.Models.DTOs;
 using WorkTracker.Services.Interfaces;
 
 namespace WorkTracker.Test.Controllers
 {
     public class ProjectControllerTest
     {
-        private readonly Mock<IAuthService> _authInterface;
-        private readonly AuthController _authController;
-
+        private readonly Mock<IProjectService> _projectInterface;
+        private readonly ProjectController _projectController;
+        private readonly AppSettings _appSettings;
+        private readonly int _demoUserId = 22;
+        private readonly string _token;
+        
         public ProjectControllerTest()
         {
-            _authInterface = new Mock<IAuthService>();
-            _authController = new AuthController(_authInterface.Object);
+            _projectInterface = new Mock<IProjectService>();
+            _projectController = new ProjectController(_projectInterface.Object);
+            
+            _appSettings = Helper.GetAppSettings();
+            var permissions = new string[]
+            {
+                "create_story",
+                "create_user",
+                "view_story",
+                "edit_story",
+                "view_project"
+            };
+            _token = WorkTracker.Services.Helper.GenerateToken(_demoUserId, permissions, _appSettings.JwtSecret);
         }
 
+        [SetUp]
+        public void Setup()
+        {
+            _projectController.ControllerContext = new ControllerContext();
+            _projectController.ControllerContext.HttpContext = new DefaultHttpContext();
+            var header = new KeyValuePair<string, StringValues>
+            (
+                "Authorization",
+                $"Bearer {_token}"
+            );
+            _projectController.ControllerContext.HttpContext.Request.Headers.Add(header);
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task GetProjectByTeamId_Missing_Token()
+        {
+            var response = new Dictionary<int, List<Project>>();
+            _projectInterface.Setup(x => x.GetByUserId(_demoUserId))
+                .ReturnsAsync(response);
+            
+            _projectController.ControllerContext = new ControllerContext();
+            _projectController.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var result = await _projectController.GetProjectByTeamId();
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+            Assert.AreEqual("User id missing", ((BadRequestObjectResult)result).Value);
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task GetProjectByTeamId_Successful()
+        {
+            var response = new Dictionary<int, List<Project>>();
+            _projectInterface.Setup(x => x.GetByUserId(_demoUserId))
+                .ReturnsAsync(response);
+
+            var result = await _projectController.GetProjectByTeamId();
+            Assert.IsInstanceOf<ActionResult>(result);
+            var value = (OkObjectResult)result;
+            Assert.IsInstanceOf<Dictionary<int, List<Project>>>(value.Value);
+        }
     }
 }
--- WorkTracker.Test/Services/ProjectServiceTest.cs
+++ WorkTracker.Test/Services/ProjectServiceTest.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Moq;
 using NUnit.Framework;
+using WorkTracker.Models.ServiceModels;
 using WorkTracker.Repositories.Interfaces;
 using WorkTracker.Services;
 
@@ -11,17 +13,65 @@ namespace WorkTracker.Test.Services
     {
         private readonly Mock<IProjectRepository> _projectRepository;
         private readonly Mock<ITeamRepository> _teamRepository;
-        private readonly Mock<IServiceLogRepository> _serviceLogRepository;
         private readonly ProjectService _projectService;
+        private readonly int _demoUserId = 22;
         public ProjectServiceTest()
         {
             _projectRepository = new Mock<IProjectRepository>();
             _teamRepository = new Mock<ITeamRepository>();
-            _serviceLogRepository = new Mock<IServiceLogRepository>();
+            var serviceLogRepository = new Mock<IServiceLogRepository>();
             _projectService = new ProjectService(_projectRepository.Object,
                 _teamRepository.Object,
-                _serviceLogRepository.Object);
+                serviceLogRepository.Object);
         }
         
+        [Test]
+        public async System.Threading.Tasks.Task GetStoriesByStateId_Successful()
+        {
+            var now = DateTime.Now;
+            var teamRepoResponse = new List<Team>
+            {
+                new Team
+                {
+                    Name = "test",
+                    OrganisationId = 1,
+                    TeamId = 1,
+                }
+            };
+            var expectedProjectResult = new Project
+            {
+                Name = "test project",
+                Description = "test desc",
+                CompletedAt = now,
+                CreatedAt = now.AddDays(-2),
+                DeletedAt = null,
+                ProjectId = 0,
+                TeamId = 1
+            };
+            var repoResponse = new List<Project>
+            {
+                expectedProjectResult
+            };
+            _teamRepository.Setup(x => x.GetByUserId(_demoUserId))
+                .ReturnsAsync(teamRepoResponse);
+            _projectRepository.Setup(x => x.GetByTeamId(1))
+                .ReturnsAsync(repoResponse);
+
+            var result = await _projectService.GetByUserId(_demoUserId);
+
+            Assert.IsInstanceOf<Dictionary<int, List<Models.DTOs.Project>>>(result);
+            Assert.IsTrue(result.ContainsKey(1));
+            Assert.Multiple(() =>
+            {
+                var project = result.GetValueOrDefault(1)?.FirstOrDefault();
+                Assert.IsTrue(project != null);
+                Assert.AreEqual(expectedProjectResult.Name, project.Name);
+                Assert.AreEqual(expectedProjectResult.Description, project.Description);
+                Assert.AreEqual(expectedProjectResult.CompletedAt, project.CompletedAt);
+                Assert.AreEqual(expectedProjectResult.CreatedAt, project.CreatedAt);
+                Assert.AreEqual(expectedProjectResult.ProjectId, project.ProjectId);
+                Assert.AreEqual(expectedProjectResult.TeamId, project.TeamId);
+            });
+        }
     }
 }
